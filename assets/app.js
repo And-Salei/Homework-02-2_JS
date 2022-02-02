14e5084d031e6ce3cd0c20ba4d7bd87e52108447
And-Salei <saleiandrew@gmail.com>
2022-02-02 22:46:58 +0200

    Start

---
+++ assets/app.js
@@ -0,0 +1,55 @@
+
+let text = prompt ('Введите сумму');
+
+
+const alphabet = {
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliet',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'X-ray',
+    'Y': 'Yankee',
+    'Z': 'Zulu',
+}
+
+for (let item in alphabet) {if (item===text) 
+    {console.log (alphabet[item])}}
+
+
+const number1 = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Fouth',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+    '0': 'Zero',
+}
+
+for (let item in number1) {if (item===text) 
+    {console.log (number1[item])}}
+
+
+console.log (alphabet[text[0]],alphabet[text[1]],number1[text[2]],number1[text[3]],number1[text[4]],number1[text[5]]);
